4e43958bfb1edef1521b54941011193525aed47b
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2024-04-28 16:05:49 +0300

    redbull: remove useless condition from 32-bit cpu config
    * Since we got rid of those product targets in device-specific trees and want to build the 32 bit targets regardless

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -26,13 +26,12 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
 
-ifeq (,$(filter %_64,$(TARGET_PRODUCT)))
+# Build the 32 bit targets
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
-endif
 
 BUILD_BROKEN_DUP_RULES := true
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 printk.devkmsg=on
